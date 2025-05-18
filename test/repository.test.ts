1eb7efa7fb7b847ce28bc3d11bb3d75d79af5fd7
riku <opa9788@gmail.com>
2025-05-18 22:55:05 +0900

    PrismaClientをmockする準備をした

---
+++ test/repository.test.ts
@@ -0,0 +1,13 @@
+import { describe, test } from "vitest";
+
+describe("memo repository" , async()=>{
+    test("fetchMemos func" , async()=> {
+
+    })
+})
+
+describe("user repository" , async()=>{
+    test("userInfoByEmail func" , async()=> {
+
+    })
+})
