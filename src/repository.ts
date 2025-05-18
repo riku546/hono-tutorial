d9678e40c397b3035889901f12e1e2f76ac3a6b3
riku <opa9788@gmail.com>
2025-05-19 03:07:11 +0900

    MemoRepositoryのunitテストを実装

--- src/repository.ts
+++ src/repository.ts
@@ -1,6 +1,4 @@
-import { PrismaClient } from "./generated/prisma/index.js";
-
-const prisma = new PrismaClient();
+import prisma from "../lib/prisma.js";
 
 export class MemoRepository {
   static fetchMemos = async (userId: string) => {
--- test/repository.test.ts
+++ test/repository.test.ts
@@ -1,13 +1,90 @@
-import { describe, test } from "vitest";
+import { describe, expect, test, vi } from "vitest";
+import prisma from "../lib/__mocks__/prisma.js";
+import { MemoRepository } from "../src/repository.js";
 
-describe("memo repository" , async()=>{
-    test("fetchMemos func" , async()=> {
+vi.mock("../lib/prisma.js");
 
-    })
-})
+describe("memo repository", async () => {
+  test("fetchMemos func", async () => {
+    //memosのcreatedAtは降順に並んでいる
+    const memos = [
+      {
+        title: "test1",
+        content: "test content1",
+        createdAt: new Date("2023-11-01T10:00:00"),
+      },
+      {
+        title: "test2",
+        content: "test content2",
+        createdAt: new Date("2023-11-02T10:00:00"),
+      },
+      {
+        title: "test3",
+        content: "test content3",
+        createdAt: new Date("2023-11-03T10:00:00"),
+      },
+    ];
 
-describe("user repository" , async()=>{
-    test("userInfoByEmail func" , async()=> {
+    const userId = "testUserId";
 
-    })
-})
+    prisma.memo.findMany.mockResolvedValue(memos as any);
+    const result = await MemoRepository.fetchMemos(userId);
+    expect(result).toEqual(memos);
+  });
+
+  test("createMemo func", async () => {
+    const newMemo = {
+      userId: "test userId",
+      title: "test title",
+      content: "test content",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    prisma.memo.create.mockResolvedValue(newMemo);
+    const result = await MemoRepository.createMemo(
+      newMemo.userId,
+      newMemo.title,
+      newMemo.content
+    );
+    expect(result).toEqual(newMemo);
+  });
+
+  test("updateMemo func", async () => {
+    const updatedMemo = {
+      title: "updated title",
+      content: "updated content",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      userId: "test userId",
+    };
+
+    prisma.memo.update.mockResolvedValue(updatedMemo);
+    const result = await MemoRepository.updateMemo(
+      updatedMemo.userId,
+      updatedMemo.title,
+      updatedMemo.content,
+      updatedMemo.createdAt
+    );
+    expect(result).toBe(updatedMemo);
+  });
+
+  test("deleteMemo func", async () => {
+    const deletedMemo = {
+      title: "test title",
+      content: "test content",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      userId: "test userId",
+    };
+
+    prisma.memo.delete.mockResolvedValueOnce(deletedMemo);
+    const result = await MemoRepository.deleteMemo(
+      deletedMemo.userId,
+      deletedMemo.createdAt
+    );
+    expect(result).toBe(deletedMemo);
+  });
+});
+
+
